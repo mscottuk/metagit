87d6aae54a26dd94113f3feb7dab0ca77b827bbb
Mark <cms4@soton.ac.uk>
2015-07-15 11:09:28 +0100

    ldc.py library: Added exceptions, tidied up constructor, and added shared get, update and print functions

--- ldc.py
+++ ldc.py
@@ -5,11 +5,11 @@ import pygit2
 class NoRepositoryError(Exception):
 	"""Could not find a Git repository"""
 	pass
-	
+
 class NoMetadataBranchError(Exception):
 	"""Could not find a metadata branch in the repository"""
 	pass
-	
+
 class MetadataBlobNotFoundError(Exception):
 	"""Could not find metadata blob in the tree"""
 	pass
@@ -18,44 +18,46 @@ class MetadataTreeNotFoundError(Exception):
 	"""Could not find metadata with path specified (tree with specified name not found)"""
 	pass
 
+class MatchingDataNotFoundError(Exception):
+	"""File does not exist in folder"""
+	pass
+
+class MetadataFileFormatError(Exception):
+	"""File is not in correct format"""
+	pass
+
+class KeyValuePairArgumentError(Exception):
+	"""Key value pair is not in key=value format"""
+	pass
+
+class FileActions:
+	dump = 1
+	json = 2
+	default = dump | json
+
+	@staticmethod
+	def is_action(action,actioncmp):
+		return action & actioncmp == actioncmp
+
 class MetadataRepo:
-	MetadataBlobNotFoundError = MetadataBlobNotFoundError
-	
-	def __init__(self):
-	
-		# Parse the user arguments
-		self.args = args
-	
-		if self.args.path is None:
-			self.req_path=os.getcwd()
-		else:
-			self.req_path=self.args.path
 
-		self.abs_req_path=os.path.abspath(self.req_path)
+	def __init__(self, req_path, branchname, storeonly, debug):
 
-		if not os.path.exists(self.abs_req_path):
-			if self.args.storeonly:
-				# File does not exist, but have been asked to look in store
-				# Will try to find a store in parent directory of requested file
-				self.repopath=os.path.dirname(self.abs_req_path)
-			else:
-				print "File does not exist in folder. Please use --storeonly to check in metadata store anyway."
-				raise IOError("File not found")
-		else:
-			# Requested path exists. Will use requested path as place to start looking for repository
-			self.repopath=self.abs_req_path
-						
-		try:
-			self.repodir = os.path.abspath(pygit2.discover_repository(self.repopath))
-		except KeyError:
-			raise NoRepositoryError("Could not find a Git repository")
+		# Save miscellaneous arguments
+		self.branchname = branchname
+		self.storeonly = storeonly
+		self.debug = debug
 
-		self.repo = pygit2.Repository(self.repodir)
-		self.basedir = os.path.dirname(self.repodir)
-		self.rel_req_path=self.abs_req_path[len(self.basedir)+1:]
-		
-		if self.args.verbose:
-			print "Repo=" + self.repodir
+		# Sort out paths
+		self.req_path = req_path
+		self.abs_req_path=os.path.abspath(req_path)
+		self.repo_path = self.get_repo_path(self.abs_req_path,storeonly)
+		self.repo = pygit2.Repository(self.repo_path)
+		self.base_path = os.path.dirname(self.repo_path)
+		self.rel_req_path=self.abs_req_path[len(self.base_path)+1:]
+
+		if debug:
+			print "Repo=" + self.repo_path
 			print "Abs=" + self.abs_req_path
 			print "Rel="+ self.rel_req_path
 
@@ -84,31 +86,29 @@ class MetadataRepo:
 				else:
 					raise MetadataTreeNotFoundError("Could not find metadata with path specified (Tree " + nextitem + " not found")
 		except KeyError, e:
-			raise MetadataBlobNotFoundError("No metadata not found for " + e.__str__())
-	
-	def get_metadata(self):
-		self.branch = self.repo.lookup_branch(self.args.branch)
+			raise MetadataBlobNotFoundError("No metadata found for " + e.__str__())
+
+	def get_metadata_blob(self):
+		self.branch = self.repo.lookup_branch(self.branchname)
 
 		if self.branch is None:
 			raise NoMetadataBranchError("Could not find metadata branch in the repository")
 		else:
 			self.commit = self.branch.get_object() # Save commit for later
 			tree = self.commit.tree
+
 			blob = self.get_blob(self.rel_req_path.split(os.sep),tree)
 			return blob
-		
-	def is_action(self,action):
-		return self.args.action & action == action
-		
+
 	def save_metadata(self,jsondict):
 		newfile = json.dumps(jsondict)
 		newblobid = self.repo.create_blob(newfile)
-		
+
 		if hasattr(self,'metadatatree'):
 			treebuilder = self.repo.TreeBuilder(self.metadatatree)
 		else:
 			treebuilder = self.repo.TreeBuilder()
-			
+
 		treebuilder.insert(self.metadatafilename,newblobid,pygit2.GIT_FILEMODE_BLOB)
 		treebuilderid = treebuilder.write()
 		print "Tree saved as " + treebuilderid.__str__()
@@ -122,3 +122,54 @@ class MetadataRepo:
 		branchref.set_target(commitid,pygit2.Signature('Mark', 'cms4@soton.ac.uk'), "Updated " + self.metadatafilename + " metadata")
 # 		branchref.log_append(commitid, pygit2.Signature('Mark', 'cms4@soton.ac.uk'), "Updated " + self.metadatafilename + " metadata")
 		print "Commit saved as " + commitid.__str__()
+
+	def get_repo_path(self,abs_req_path,storeonly):
+		if os.path.exists(abs_req_path):
+			# Requested path exists. Will use requested path as place to start looking for repository
+			repo_start_path=abs_req_path
+		elif storeonly:
+			# Path does not exist, but have been asked to look in store
+			# Will try to find a store in parent directory of requested file
+			repo_start_path=os.path.dirname(abs_req_path)
+		else:
+			# Path does not exist so we will not proceed any further
+			raise MatchingDataNotFoundError("Matching data file does not exist in folder")
+
+		try:
+			repo_path = os.path.abspath(pygit2.discover_repository(repo_start_path))
+		except KeyError:
+			raise NoRepositoryError("Could not find a Git repository")
+
+		return repo_path
+
+	def print_metadata(self,fileaction):
+		gitblob = self.get_metadata_blob()
+
+		if FileActions.is_action(fileaction,FileActions.json):
+			try:
+				self.printjson(gitblob)
+			except ValueError:
+				if FileActions.is_action(fileaction,FileActions.dump):
+					self.dumpfile(gitblob)
+				else:
+					raise MetadataFileFormatError("Not JSON data. Use --dump to show file anyway.")
+		elif FileActions.is_action(fileaction,FileActions.dump):
+			self.dumpfile(gitblob)
+
+	def printjson(self,gitblob):
+		data = json.loads(gitblob.data)
+		for key, value in data.iteritems():
+		  print '{:<20} {:<20}'.format(key,  value)
+
+	def dumpfile(self,gitblob):
+		print gitblob.data
+
+	def update_metadata(self,k,v):
+		try:
+			blob = self.get_metadata_blob()
+			blobdata = json.loads(blob.data)
+		except MetadataBlobNotFoundError, e:
+			blobdata = json.loads("{}")
+
+		blobdata[k] = v
+		self.save_metadata(blobdata)
